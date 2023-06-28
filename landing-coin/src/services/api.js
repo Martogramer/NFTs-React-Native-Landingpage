089fa2a2776bf6d27060c6748c962c685b8a08a9
Martin Iribas <martin.iribass@gmail.com>
2023-06-28 01:16:21 -0300

    feat(services&state): conect to api back

---
+++ landing-coin/src/services/api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001'; // Reemplaza con la URL de tu API backend
+
+// Función para obtener todos los destinos turísticos
+export const getDestinations = async () => {
+  const response = await axios.get(`${BASE_URL}/destinations`);
+  console.log("data")
+  return response.data;
+};
+
+// Función para crear un nuevo destino turístico
+export const createDestination = async (destination) => {
+  const response = await axios.post(`${BASE_URL}/destinations`, destination);
+  return response.data;
+};
+
+// Función para obtener un destino turístico por su ID
+export const getDestinationById = async (id) => {
+  const response = await axios.get(`${BASE_URL}/destinations/${id}`);
+  return response.data;
+};
+
+// Función para buscar destinos turísticos por nombre o ubicación
+export const searchDestinations = async (query) => {
+  const response = await axios.get(`${BASE_URL}/destinations`, {
+    params: { name: query },
+  });
+  return response.data;
+};
+
+// delete
+export const deleteDestination = async (id) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/destinations/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
